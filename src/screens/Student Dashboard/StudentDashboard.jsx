2a817cfeccd0514e6751d9f7f1f6432aa613b96c
Farhan Gani <farhangani.sheikh@gokhana.com>
2023-10-07 03:29:40 +0530

    used synthesia

--- src/screens/Student Dashboard/StudentDashboard.jsx
+++ src/screens/Student Dashboard/StudentDashboard.jsx
@@ -16,55 +16,128 @@ export default function StudentDashboard() {
   const [videoUrl, setVideoUrl] = useState("");
 
   const createVideo = async () => {
-    const API_KEY = "c2FpbGlraGl0aDEwOEBnbWFpbC5jb20:UY58mosKaXVN27C6x13DC";
+    // ******************** DID ****************
+    // const API_KEY = "YOUR_API_KEY";
 
-    const response = await axios.post(
-      "https://api.d-id.com/talks",
-      {
-        script: {
-          type: "text",
-          subtitles: "false",
-          provider: {
-            type: "microsoft",
-            voice_id: "en-US-JennyNeural",
-          },
-          ssml: "false",
-          input:
-            "Hi my name is farhan and i am doing this test for avatar creation for mock interviews... are you listening to me?",
-        },
-        config: {
-          fluent: "false",
-          pad_audio: "0.0",
-        },
-        source_url:
-          'https://clips-presenters.d-id.com/amy/FLZ1USJl7m/vzswgDCwKZ/image.png',
+    // const response = await axios.post(
+    //   "https://api.d-id.com/talks",
+    //   {
+    //     script: {
+    //       type: "text",
+    //       subtitles: "false",
+    //       provider: {
+    //         type: "microsoft",
+    //         voice_id: "en-US-JennyNeural",
+    //       },
+    //       ssml: "false",
+    //       input:
+    //         "Hi my name is farhan and i am doing this test for avatar creation for mock interviews... are you listening to me?",
+    //     },
+    //     config: {
+    //       fluent: "false",
+    //       pad_audio: "0.0",
+    //     },
+    //     source_url:
+    //       "https://clips-presenters.d-id.com/amy/FLZ1USJl7m/vzswgDCwKZ/image.png",
+    //   },
+    //   {
+    //     headers: {
+    //       accept: "application/json",
+    //       authorization:
+    //       "YOUR_API_KEY",
+    //       "content-type": "application/json",
+    //     },
+    //   }
+    // );
+    // ********************** DID ***************
+
+    // ******************** Syntesia ****************
+
+    const API_KEY = "YOUR_API_KEY"; // Replace with your actual API key
+    const apiUrl = "https://api.synthesia.io/v2/videos";
+
+    const requestData = {
+      test: true,
+      title: "Hello, World!",
+      description:
+        "This is my first synthetic video, made with the Synthesia API!",
+      visibility: "public",
+      ctaSettings: {
+        label: "Click me!",
+        url: "https://www.synthesia.io",
       },
-      {
-        headers: {
-          accept: "application/json",
-          authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik53ek53TmV1R3ptcFZTQjNVZ0J4ZyJ9.eyJodHRwczovL2QtaWQuY29tL2ZlYXR1cmVzIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJvZHVjdF9pZCI6IiIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX2N1c3RvbWVyX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJvZHVjdF9uYW1lIjoidHJpYWwiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9zdWJzY3JpcHRpb25faWQiOiIiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9iaWxsaW5nX2ludGVydmFsIjoibW9udGgiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9wbGFuX2dyb3VwIjoiZGVpZC10cmlhbCIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX3ByaWNlX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJpY2VfY3JlZGl0cyI6IiIsImh0dHBzOi8vZC1pZC5jb20vY2hhdF9zdHJpcGVfc3Vic2NyaXB0aW9uX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9jaGF0X3N0cmlwZV9wcmljZV9jcmVkaXRzIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9jaGF0X3N0cmlwZV9wcmljZV9pZCI6IiIsImh0dHBzOi8vZC1pZC5jb20vcHJvdmlkZXIiOiJhdXRoMCIsImh0dHBzOi8vZC1pZC5jb20vaXNfbmV3IjpmYWxzZSwiaHR0cHM6Ly9kLWlkLmNvbS9hcGlfa2V5X21vZGlmaWVkX2F0IjoiMjAyMy0xMC0wNFQxOTo0ODoyNy42MDFaIiwiaHR0cHM6Ly9kLWlkLmNvbS9vcmdfaWQiOiIiLCJodHRwczovL2QtaWQuY29tL2FwcHNfdmlzaXRlZCI6WyJTdHVkaW8iXSwiaHR0cHM6Ly9kLWlkLmNvbS9jeF9sb2dpY19pZCI6IiIsImh0dHBzOi8vZC1pZC5jb20vY3JlYXRpb25fdGltZXN0YW1wIjoiMjAyMy0xMC0wNFQxOTo0NDo1My42NTZaIiwiaHR0cHM6Ly9kLWlkLmNvbS9hcGlfZ2F0ZXdheV9rZXlfaWQiOiJtc3AyaGxjbno0IiwiaHR0cHM6Ly9kLWlkLmNvbS9oYXNoX2tleSI6InBsTGc2X3p4NWdhdGZmblM2VU5xUCIsImh0dHBzOi8vZC1pZC5jb20vcHJpbWFyeSI6dHJ1ZSwiaHR0cHM6Ly9kLWlkLmNvbS9lbWFpbCI6InNhaWxpa2hpdGgxMDhAZ21haWwuY29tIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmQtaWQuY29tLyIsInN1YiI6ImF1dGgwfDY1MWRjMGI1MGQ4NWE3OWE0Y2MzYzFkNSIsImF1ZCI6WyJodHRwczovL2QtaWQudXMuYXV0aDAuY29tL2FwaS92Mi8iLCJodHRwczovL2QtaWQudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY5NjQ0ODk3NCwiZXhwIjoxNjk2NTM1Mzc0LCJhenAiOiJHenJOSTFPcmU5Rk0zRWVEUmYzbTN6M1RTdzBKbFJZcSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgcmVhZDpjdXJyZW50X3VzZXIgdXBkYXRlOmN1cnJlbnRfdXNlcl9tZXRhZGF0YSBvZmZsaW5lX2FjY2VzcyJ9.ip6CJS0Co36OrKzsPcKW_WAR1-hhRmOF2RtIYNY8fjvd9bZt-eM99GjSCO0q5qt0lk0PVUlz6q5Dbjxxafav5zS0tBJaDdA3rP5AQnn5ci1coMUQ7GVpsnWfkIofiAFKdaEpuWlfPPW4bmMyWzMP43fSidhiShn1kgNBH2xk1uDTrUvzsaAXlhLTZyO1NzXezuCSney4fJTmjW0ePWoWfpp3tu3hs03DpBZv4VAes--y3b8T4CEf75Bcd4oO9ldr5-u-PjTzZGBzp4_KfPyHYrBefT0c7xXcuBEdvb8cXCpS4ZF4w_n91PAL810QQC0Q497TfuCxRcv7QKaLp2d-lg`,
-          "content-type": "application/json",
+      callbackId: "email@example.com",
+      input: [
+        {
+          scriptText:
+            "This is my first synthetic video, made with the Synthesia API!",
+          avatar: "anna_costume1_cameraA",
+          avatarSettings: {
+            voice: "1364e02b-bdae-4d39-bc2d-6c4a34814968",
+            horizontalAlign: "center",
+            scale: 1.0,
+            style: "rectangular",
+          },
+          background: "off_white",
         },
-      }
-    );
-    setVideoId(response.data.id);
+      ],
+      soundtrack: "urban",
+    };
+
+    axios
+      .post(apiUrl, requestData, {
+        headers: {
+          Authorization: API_KEY,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log("Video creation response:", response.data);
+        setVideoId(response.data.id);
+      })
+      .catch((error) => {
+        console.error("Error creating video:", error);
+      });
   };
 
   const getVideo = async () => {
-    const url = 'https://api.d-id.com/talks/tlk_9TrJ-j1j8O_xT7ng6CSO5';
-const headers = {
-  accept: 'application/json',
-    authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik53ek53TmV1R3ptcFZTQjNVZ0J4ZyJ9.eyJodHRwczovL2QtaWQuY29tL2ZlYXR1cmVzIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJvZHVjdF9pZCI6IiIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX2N1c3RvbWVyX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJvZHVjdF9uYW1lIjoidHJpYWwiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9zdWJzY3JpcHRpb25faWQiOiIiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9iaWxsaW5nX2ludGVydmFsIjoibW9udGgiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9wbGFuX2dyb3VwIjoiZGVpZC10cmlhbCIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX3ByaWNlX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJpY2VfY3JlZGl0cyI6IiIsImh0dHBzOi8vZC1pZC5jb20vY2hhdF9zdHJpcGVfc3Vic2NyaXB0aW9uX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9jaGF0X3N0cmlwZV9wcmljZV9jcmVkaXRzIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9jaGF0X3N0cmlwZV9wcmljZV9pZCI6IiIsImh0dHBzOi8vZC1pZC5jb20vcHJvdmlkZXIiOiJhdXRoMCIsImh0dHBzOi8vZC1pZC5jb20vaXNfbmV3IjpmYWxzZSwiaHR0cHM6Ly9kLWlkLmNvbS9hcGlfa2V5X21vZGlmaWVkX2F0IjoiMjAyMy0xMC0wNFQxOTo0ODoyNy42MDFaIiwiaHR0cHM6Ly9kLWlkLmNvbS9vcmdfaWQiOiIiLCJodHRwczovL2QtaWQuY29tL2FwcHNfdmlzaXRlZCI6WyJTdHVkaW8iXSwiaHR0cHM6Ly9kLWlkLmNvbS9jeF9sb2dpY19pZCI6IiIsImh0dHBzOi8vZC1pZC5jb20vY3JlYXRpb25fdGltZXN0YW1wIjoiMjAyMy0xMC0wNFQxOTo0NDo1My42NTZaIiwiaHR0cHM6Ly9kLWlkLmNvbS9hcGlfZ2F0ZXdheV9rZXlfaWQiOiJtc3AyaGxjbno0IiwiaHR0cHM6Ly9kLWlkLmNvbS9oYXNoX2tleSI6InBsTGc2X3p4NWdhdGZmblM2VU5xUCIsImh0dHBzOi8vZC1pZC5jb20vcHJpbWFyeSI6dHJ1ZSwiaHR0cHM6Ly9kLWlkLmNvbS9lbWFpbCI6InNhaWxpa2hpdGgxMDhAZ21haWwuY29tIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmQtaWQuY29tLyIsInN1YiI6ImF1dGgwfDY1MWRjMGI1MGQ4NWE3OWE0Y2MzYzFkNSIsImF1ZCI6WyJodHRwczovL2QtaWQudXMuYXV0aDAuY29tL2FwaS92Mi8iLCJodHRwczovL2QtaWQudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY5NjQ0ODk3NCwiZXhwIjoxNjk2NTM1Mzc0LCJhenAiOiJHenJOSTFPcmU5Rk0zRWVEUmYzbTN6M1RTdzBKbFJZcSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgcmVhZDpjdXJyZW50X3VzZXIgdXBkYXRlOmN1cnJlbnRfdXNlcl9tZXRhZGF0YSBvZmZsaW5lX2FjY2VzcyJ9.ip6CJS0Co36OrKzsPcKW_WAR1-hhRmOF2RtIYNY8fjvd9bZt-eM99GjSCO0q5qt0lk0PVUlz6q5Dbjxxafav5zS0tBJaDdA3rP5AQnn5ci1coMUQ7GVpsnWfkIofiAFKdaEpuWlfPPW4bmMyWzMP43fSidhiShn1kgNBH2xk1uDTrUvzsaAXlhLTZyO1NzXezuCSney4fJTmjW0ePWoWfpp3tu3hs03DpBZv4VAes--y3b8T4CEf75Bcd4oO9ldr5-u-PjTzZGBzp4_KfPyHYrBefT0c7xXcuBEdvb8cXCpS4ZF4w_n91PAL810QQC0Q497TfuCxRcv7QKaLp2d-lg`,
-};
+    // ******************** DID ****************
+    // const url = `https://api.d-id.com/talks/${videoId}`;
+    // const headers = {
+    //   accept: "application/json",
+    //   authorization:
+    //     "YOUR_API_KEY",
+    // };
 
-axios.get(url, { headers })
-  .then(response => {
-    setVideoUrl(response.data.result_url);
-  })
-  .catch(error => {
-    console.error(error);
-  });
-    
+    // axios
+    //   .get(url, { headers })
+    //   .then((response) => {
+    //     setVideoUrl(response.data.result_url);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+
+    // ******************** DID ****************
+
+    // ******************** synthesia ****************
+
+    const API_KEY = "YOUR_API_KEY"; // Replace with your actual API key// Replace with the video ID you want to retrieve
+    const apiUrl = `https://api.synthesia.io/v2/videos/${videoId}`;
+
+    axios
+      .get(apiUrl, {
+        headers: {
+          Authorization: API_KEY,
+        },
+      })
+      .then((response) => {
+        console.log("Video retrieval response:", response.data);
+        // setVideoUrl(response.data.result_url);
+      })
+      .catch((error) => {
+        console.error("Error retrieving video:", error);
+      });
   };
 
   const navigate = useNavigate();
@@ -72,20 +145,18 @@ axios.get(url, { headers })
 
   useEffect(() => {
     dispatch(loadUserStats());
-    // createVideo();
-    getVideo()
+    createVideo();
   }, []);
 
-//   useEffect(() => {
-//       getVideo()
-//   },[videoId])
+  useEffect(() => {
+    getVideo();
+  }, [videoId]);
 
-  console.log(videoUrl)
+  console.log(videoUrl);
 
   return (
     <div>
-        <video controls src={videoUrl} width="640" height="360" autoPlay>
-    </video>
+      <video controls src={videoUrl} width="640" height="360" autoPlay></video>
       <div
         className="grid grid-cols-2 gap-7 px-6 py-6"
         style={{ "grid-template-columns": "64% 34%" }}
