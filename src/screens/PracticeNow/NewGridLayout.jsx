e430d6a5808cdc2dc08f2bfae7dee32bf1ab0608
Farhan Gani <farhangani.sheikh@gokhana.com>
2023-10-08 02:47:05 +0530

    interview layout

--- src/screens/PracticeNow/NewGridLayout.jsx
+++ src/screens/PracticeNow/NewGridLayout.jsx
@@ -1,17 +1,141 @@
-import React from "react";
-import videoSrc from "../../assets/caption.mp4"
-import person1 from "../../assets/download.jpeg"
+import React, { useEffect } from "react";
+import videoSrc from "../../assets/caption.mp4";
+import person1 from "../../assets/download.jpeg";
+import Button from "@mui/material/Button";
+import { useRecordWebcam } from "react-record-webcam";
 export default function NewGridLayout() {
-  return <div className="p-5 grid grid-cols-5 gap-3 h-full" >
-    <div className=" col-span-4" >
-        <video src={videoSrc}  autoPlay controls></video>
-    </div>
-    {/* <div className=" col-span-1 grid gap-3 overflow-y-auto" >
-    {
-        [1,1,1,1,1,1]?.map((_,i)=>(
-        <div key={i} className="bg-blue-500 rounded-xl min-h-[150px]" style={{background:`url(${person1})`,backgroundPosition:"center",backgroundSize:"cover",backgroundRepeat:"no-repeat"}} > </div> 
-        ))
+  const {
+    activeRecordings,
+    createRecording,
+    openCamera,
+    startRecording,
+    stopRecording,
+  } = useRecordWebcam();
+
+  const recordWebcam = useRecordWebcam({ frameRate: 60 });
+  console.log(recordWebcam, "recordWebcam");
+  const getSource = () => {
+    return "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3";
+  };
+
+  // useEffect(() => {
+  //   handleStartRecording()
+  // },[])
+  const handleStartRecording = async () => {
+    try {
+      const recording = await createRecording();
+      console.log(recording, "recording");
+      if (!recording) {
+        return;
+      }
+      await openCamera(recording.id);
+      await startRecording(recording.id);
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      await stopRecording(recording.id);
+    } catch (error) {
+      console.error({ error });
     }
-    </div> */}
-  </div>;
+  };
+
+  return (
+    <>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: 900,
+            marginBottom: "10px",
+          }}
+        >
+          <Button variant="contained">End Interview </Button>
+          <Button variant="contained">Skip Question </Button>
+          <Button variant="contained">Repeat Question </Button>
+          {/* re record answer ------ */}
+        </div>
+        <div
+          style={{
+            width: 400,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: "10px",
+          }}
+        >
+          <Button variant="contained">i Have finished answering </Button>
+          {/* Next Question */}
+        </div>
+      </div>
+      <div style={{ display: "flex" }}>
+        <div className="">
+          <img
+            src={person1}
+            style={{ width: 900, height: 500, objectFit: "cover" }}
+          />
+          <audio controls>
+            <source src="https://www.computerhope.com/jargon/m/example.mp3" />
+          </audio>
+        </div>
+        <div
+          style={{
+            background: "rgb(219 234 254)",
+            width: 400,
+            height: 500,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              background: "rgb(126 150 184)",
+              padding: "10px 15px",
+              borderRadius: "10px",
+              marginTop: 10,
+              marginBottom: 20,
+            }}
+          >
+            Standby
+          </div>
+          {/* prep record  */}
+
+          <div>Interview</div>
+          <div
+            style={{
+              background: "rgb(126 150 184)",
+              padding: "10px 15px",
+              borderRadius: "10px",
+              marginBottom: 20,
+            }}
+          >
+            10:00
+          </div>
+          <div>Question</div>
+          <div
+            style={{
+              background: "rgb(126 150 184)",
+              padding: "10px 15px",
+              borderRadius: "10px",
+              marginBottom: 20,
+            }}
+          >
+            5:00
+          </div>
+          <button onClick={handleStartRecording}>Start</button>
+          <div className="recordings">
+            {activeRecordings.map((recording) => {
+              return (
+                <>
+                <div key={recording.id}>
+                  <video ref={recording.webcamRef} autoPlay muted />
+                </div>
+                 </>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/screens/Student Dashboard/StudentDashboard.jsx
+++ src/screens/Student Dashboard/StudentDashboard.jsx
@@ -11,152 +11,15 @@ import { useDispatch } from "react-redux";
 import axios from "axios";
 
 export default function StudentDashboard() {
-  console.log("student dashboard");
-  const [videoId, setVideoId] = useState("");
-  const [videoUrl, setVideoUrl] = useState("");
-
-  const createVideo = async () => {
-    // ******************** DID ****************
-    // const API_KEY = "YOUR_API_KEY";
-
-    // const response = await axios.post(
-    //   "https://api.d-id.com/talks",
-    //   {
-    //     script: {
-    //       type: "text",
-    //       subtitles: "false",
-    //       provider: {
-    //         type: "microsoft",
-    //         voice_id: "en-US-JennyNeural",
-    //       },
-    //       ssml: "false",
-    //       input:
-    //         "Hi my name is farhan and i am doing this test for avatar creation for mock interviews... are you listening to me?",
-    //     },
-    //     config: {
-    //       fluent: "false",
-    //       pad_audio: "0.0",
-    //     },
-    //     source_url:
-    //       "https://clips-presenters.d-id.com/amy/FLZ1USJl7m/vzswgDCwKZ/image.png",
-    //   },
-    //   {
-    //     headers: {
-    //       accept: "application/json",
-    //       authorization:
-    //       "YOUR_API_KEY",
-    //       "content-type": "application/json",
-    //     },
-    //   }
-    // );
-    // ********************** DID ***************
-
-    // ******************** Syntesia ****************
-
-    const API_KEY = "YOUR_API_KEY"; // Replace with your actual API key
-    const apiUrl = "https://api.synthesia.io/v2/videos";
-
-    const requestData = {
-      test: true,
-      title: "Hello, World!",
-      description:
-        "This is my first synthetic video, made with the Synthesia API!",
-      visibility: "public",
-      ctaSettings: {
-        label: "Click me!",
-        url: "https://www.synthesia.io",
-      },
-      callbackId: "email@example.com",
-      input: [
-        {
-          scriptText:
-            "This is my first synthetic video, made with the Synthesia API!",
-          avatar: "anna_costume1_cameraA",
-          avatarSettings: {
-            voice: "1364e02b-bdae-4d39-bc2d-6c4a34814968",
-            horizontalAlign: "center",
-            scale: 1.0,
-            style: "rectangular",
-          },
-          background: "off_white",
-        },
-      ],
-      soundtrack: "urban",
-    };
-
-    axios
-      .post(apiUrl, requestData, {
-        headers: {
-          Authorization: API_KEY,
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => {
-        console.log("Video creation response:", response.data);
-        setVideoId(response.data.id);
-      })
-      .catch((error) => {
-        console.error("Error creating video:", error);
-      });
-  };
-
-  const getVideo = async () => {
-    // ******************** DID ****************
-    // const url = `https://api.d-id.com/talks/${videoId}`;
-    // const headers = {
-    //   accept: "application/json",
-    //   authorization:
-    //     "YOUR_API_KEY",
-    // };
-
-    // axios
-    //   .get(url, { headers })
-    //   .then((response) => {
-    //     setVideoUrl(response.data.result_url);
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
-
-    // ******************** DID ****************
-
-    // ******************** synthesia ****************
-
-    const API_KEY = "YOUR_API_KEY"; // Replace with your actual API key// Replace with the video ID you want to retrieve
-    const apiUrl = `https://api.synthesia.io/v2/videos/${videoId}`;
-
-    axios
-      .get(apiUrl, {
-        headers: {
-          Authorization: API_KEY,
-        },
-      })
-      .then((response) => {
-        console.log("Video retrieval response:", response.data);
-        // setVideoUrl(response.data.result_url);
-      })
-      .catch((error) => {
-        console.error("Error retrieving video:", error);
-      });
-  };
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadUserStats());
-    createVideo();
   }, []);
-
-  useEffect(() => {
-    getVideo();
-  }, [videoId]);
-
-  console.log(videoUrl);
 
   return (
     <div>
-      <video controls src={videoUrl} width="640" height="360" autoPlay></video>
       <div
         className="grid grid-cols-2 gap-7 px-6 py-6"
         style={{ "grid-template-columns": "64% 34%" }}
