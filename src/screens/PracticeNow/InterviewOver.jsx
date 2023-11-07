154eb22a814fe8fe14a6fd8df11e15d4308add8d
Sai Likhith Kanuparthi <slk522@nyu.edu>
2023-11-07 14:56:01 -0600

    updated reports

--- src/screens/PracticeNow/InterviewOver.jsx
+++ src/screens/PracticeNow/InterviewOver.jsx
@@ -58,7 +58,7 @@ const InterviewOver = () => {
                         <svg className="w-6 h-6 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
                         </svg>
-                        My Interviews
+                        My Interview Reports
                     </button>
                 </div>
             </div>
