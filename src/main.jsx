50cffded834f3170887d8e254b971a973904bca7
Ram T <ramt@Rams-MacBook-Air.local>
2023-12-17 07:52:18 -0600

    added changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { DarkModeProvider } from './Dark.jsx'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
+  <DarkModeProvider>
     <App />
-  </React.StrictMode>
+  </DarkModeProvider>
+</React.StrictMode>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,31 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        custom1: ["'Playfair Display'","'Noto Sans JP'",  "serif" ,"sans-serif",],
+        custom1: ["'Playfair Display'","'Noto Sans JP'",  "serif" ,"sans-serif"],
+      },
+      colors: {
+        // Normal Mode Colors
+        primary: "#f3f0f9", // Example primary color
+        secondary: "#000000", // Example secondary color
+
+        // Dark Mode Colors
+        dark: {
+          primary: "#071437", // Example dark mode primary color
+          secondary: "#ff6600", // Example dark mode secondary color
+        },
+      },
+      boxShadow: {
+        // Normal Mode Box Shadows
+        "sm": "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+        "md": "0 4px 6px -1px rgba(0, 0, 0, 0.1)",
+        "lg": "0 10px 15px -3px rgba(0, 0, 0, 0.1)",
+
+        // Dark Mode Box Shadows
+        "dark-sm": "0 1px 2px 0 rgba(255, 255, 255, 0.1)",
+        "dark-md": "0 4px 6px -1px rgba(255, 255, 255, 0.2)",
+        "dark-lg": "0 10px 15px -3px rgba(255, 255, 255, 0.2)",
       },
     },
   },
-  plugins: [require('@tailwindcss/forms'),],
+  plugins: [require('@tailwindcss/forms')],
 }
-
