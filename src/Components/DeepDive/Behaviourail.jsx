3a2a5c572b43cec6e416dc5206bb752ab4dad8b0
[200~ <purnimavoruganti12@gmail.com>
2023-12-31 11:35:29 +0530

    deepdivchanges

--- src/Components/DeepDive/Behaviourail.jsx
+++ src/Components/DeepDive/Behaviourail.jsx
@@ -1,7 +1,299 @@
 import React from "react";
-
+import "./Behaviourail.css";
+import { AiOutlineLike } from "react-icons/ai";
+import { FaRegThumbsDown } from "react-icons/fa";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faThumpsUp } from "@fortawesome/sharp-regular-svg-icons";
 const Behaviourail = () => {
-  return <div>Behaviourail</div>;
+  return (
+    <div>
+      <div className="behvaioural-container">
+        <h1>Behavioural Questions DeepDive</h1>
+      </div>
+      <div className="Concepts-roi">1. Explain the Concepts of ROI</div>
+
+      <div className="s-container">
+        <div className="Answers-container">
+          <h3 className="heading-1">Arpitha's Answer :</h3>
+          <br />
+
+          <p className="para-01">
+            Stop in budgeting basically return on investment is nothing but the
+            returns which we got after a particular investment for suppose if we
+            invest 1 lakh on land or property for setting up the business before
+            going to through the process if we calculate the Returns on how much
+            money we get after testing one it is called as the concept of
+            budgeting
+          </p>
+        </div>
+        <div className="Reference-container">
+          <h3 className="heading-1">Sample Answer for reference</h3>
+          <br />
+          <p className="para-01">
+            "At my previous job, we faced a situation where a critical HR
+            transformation project was at risk due to unexpected budget cuts.
+            Instead of giving up, I took the initiative to reassess our
+            resources and recalibrate our project plan. We streamlined our
+            activities, prioritized tasks more effectively, and managed to
+            complete the project successfully under budget. This experience has
+            honed my resilience and ability to adapt to challenging
+            circumstances."
+          </p>
+        </div>
+      </div>
+      <div className="insights-container">
+        <h2 className="insights">Insights</h2>
+
+        <div className="colors-container">
+          <div className="green-colors">
+            <AiOutlineLike color="green" className="dd" />
+
+            <p className="colored">what you got right</p>
+          </div>
+
+          <div className="brown-colors">
+            <FaRegThumbsDown color="brown" />
+            <span className="colorbrownds">
+              What you got wrong/what you missed
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="some-points-container">
+        <div className="colorGreen">
+          <p className="greenPara">
+            Sam correctly defined ROl as the returns obtained after a particular
+            investment
+          </p>
+        </div>
+        <div className="colorPink">
+          <p className="pinkPara">
+            Sam did not mention the specific calculation of ROl, which is
+            typically expressed as a percentage.
+          </p>
+        </div>
+      </div>
+      <div className="feedbackss">
+        <h1 className="fedbackHeads">Feedback for the Candidate :</h1>
+        <br />
+        <ol>
+          <li>
+            1.Expand on the concept of ROl by mentioning that it is calculated
+            by dividing the net profit from an investment by the initial
+            investment amount.
+          </li>
+          <li>
+            2.Provide examples of how ROl is used in budgeting, such as
+            determining the profitability of different investment options
+          </li>
+          <li>
+            3.Emphasize the importance of considering both the financial returns
+            and the time frame when calculating ROl.
+          </li>
+          <li>
+            4.Highlight the significance of ROl in decision-making processes, as
+            it helps prioritize investments based on their potential returns.{" "}
+          </li>
+
+          <li>
+            5.Encourage the candidate to research and understand different
+            methods of calculating ROl, such as the payback period and
+            discounted cash flow analysis.
+          </li>
+        </ol>
+      </div>
+      <div className="Technical-container">
+        <h1>Technical Questions DeepDive</h1>
+      </div>
+      <div className="Conceptsrois">1. Explain the Concepts of ROI</div>
+
+      <div className="s-container">
+        <div className="Answers-container">
+          <h3 className="heading-1">Arpitha's Answer :</h3>
+          <br />
+
+          <p className="para-01">
+            Stop in budgeting basically return on investment is nothing but the
+            returns which we got after a particular investment for suppose if we
+            invest 1 lakh on land or property for setting up the business before
+            going to through the process if we calculate the Returns on how much
+            money we get after testing one it is called as the concept of
+            budgeting
+          </p>
+        </div>
+        <div className="Reference-container">
+          <h3 className="heading-1">Sample Answer for reference</h3>
+          <br />
+          <p className="para-01">
+            "At my previous job, we faced a situation where a critical HR
+            transformation project was at risk due to unexpected budget cuts.
+            Instead of giving up, I took the initiative to reassess our
+            resources and recalibrate our project plan. We streamlined our
+            activities, prioritized tasks more effectively, and managed to
+            complete the project successfully under budget. This experience has
+            honed my resilience and ability to adapt to challenging
+            circumstances."
+          </p>
+        </div>
+      </div>
+      <div className="insights-container">
+        <h2 className="insights">Insights</h2>
+
+        <div className="colors-container">
+          <div className="green-colors">
+            <AiOutlineLike color="green" className="dd" />
+
+            <p className="colored">what you got right</p>
+          </div>
+
+          <div className="brown-colors">
+            <FaRegThumbsDown color="brown" />
+            <span className="colorbrownds">
+              What you got wrong/what you missed
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="some-points-container">
+        <div className="colorGreen">
+          <p className="greenPara">
+            Sam correctly defined ROl as the returns obtained after a particular
+            investment
+          </p>
+        </div>
+        <div className="colorPink">
+          <p className="pinkPara">
+            Sam did not mention the specific calculation of ROl, which is
+            typically expressed as a percentage.
+          </p>
+        </div>
+      </div>
+      <div className="feedbackss">
+        <h1 className="fedbackHeads">Feedback for the Candidate :</h1>
+        <br />
+        <ol>
+          <li>
+            1.Expand on the concept of ROl by mentioning that it is calculated
+            by dividing the net profit from an investment by the initial
+            investment amount.
+          </li>
+          <li>
+            2.Provide examples of how ROl is used in budgeting, such as
+            determining the profitability of different investment options
+          </li>
+          <li>
+            3.Emphasize the importance of considering both the financial returns
+            and the time frame when calculating ROl.
+          </li>
+          <li>
+            4.Highlight the significance of ROl in decision-making processes, as
+            it helps prioritize investments based on their potential returns.{" "}
+          </li>
+
+          <li>
+            5.Encourage the candidate to research and understand different
+            methods of calculating ROl, such as the payback period and
+            discounted cash flow analysis.
+          </li>
+        </ol>
+      </div>
+      <div className="Practical-container">
+        <h1>Practical Thinking Questions DeepDive</h1>
+      </div>
+      <div className="Conceptsroist">1. Explain the Concepts of ROI</div>
+
+      <div className="s-container">
+        <div className="Answers-container">
+          <h3 className="heading-1">Arpitha's Answer :</h3>
+          <br />
+
+          <p className="para-01">
+            stop in budgeting basically return on investment is nothing but the
+            returns which we got after a particular investment for suppose if we
+            invest 1 lakh on land or property for setting up the business before
+            going to through the process if we calculate the Returns on how much
+            money we get after testing one it is called as the concept of
+            budgeting
+          </p>
+        </div>
+        <div className="Reference-container">
+          <h3 className="heading-1">Sample Answer for reference</h3>
+          <br />
+          <p className="para-01">
+            "At my previous job, we faced a situation where a critical HR
+            transformation project was at risk due to unexpected budget cuts.
+            Instead of giving up, I took the initiative to reassess our
+            resources and recalibrate our project plan. We streamlined our
+            activities, prioritized tasks more effectively, and managed to
+            complete the project successfully under budget. This experience has
+            honed my resilience and ability to adapt to challenging
+            circumstances."
+          </p>
+        </div>
+      </div>
+      <div className="insights-container">
+        <h2 className="insights">Insights</h2>
+
+        <div className="colors-container">
+          <div className="green-colors">
+            <AiOutlineLike color="green" className="dd" />
+
+            <p className="colored">what you got right</p>
+          </div>
+
+          <div className="brown-colors">
+            <FaRegThumbsDown color="brown" />
+            <span className="colorbrownds">
+              What you got wrong/what you missed
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="some-points-container">
+        <div className="colorGreen">
+          <p className="greenPara">
+            Sam correctly defined ROl as the returns obtained after a particular
+            investment
+          </p>
+        </div>
+        <div className="colorPink">
+          <p className="pinkPara">
+            Sam did not mention the specific calculation of ROl, which is
+            typically expressed as a percentage.
+          </p>
+        </div>
+      </div>
+      <div className="feedbackss">
+        <h1 className="fedbackHeads">Feedback for the Candidate :</h1>
+        <br />
+        <ol>
+          <li>
+            1.Expand on the concept of ROl by mentioning that it is calculated
+            by dividing the net profit from an investment by the initial
+            investment amount.
+          </li>
+          <li>
+            2.Provide examples of how ROl is used in budgeting, such as
+            determining the profitability of different investment options
+          </li>
+          <li>
+            3.Emphasize the importance of considering both the financial returns
+            and the time frame when calculating ROl.
+          </li>
+          <li>
+            4.Highlight the significance of ROl in decision-making processes, as
+            it helps prioritize investments based on their potential returns.{" "}
+          </li>
+
+          <li>
+            5.Encourage the candidate to research and understand different
+            methods of calculating ROl, such as the payback period and
+            discounted cash flow analysis.
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 };
 
 export default Behaviourail;
