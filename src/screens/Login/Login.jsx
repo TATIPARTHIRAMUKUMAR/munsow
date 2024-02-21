75113e3f6bf0a1bd4313f01408f76163c2f06ef2
Farhan Gani <farhangani619@gmail.com>
2024-02-21 21:50:04 +0530

    implemented login when clicked enter button

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -10,25 +10,31 @@ import TextField from "@mui/material/TextField";
 import { useDispatch } from "react-redux";
 import { institution_login } from "../../redux/action";
 import ForgotPassword from "./ForgotPassword";
-import { Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from "@mui/material";
+import {
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 const StyledRadioGroup = styled(RadioGroup)({
-  flexDirection: 'row',
-  justifyContent: 'center',
-  marginBottom: '30px',
+  flexDirection: "row",
+  justifyContent: "center",
+  marginBottom: "30px",
 });
 
 const StyledFormControlLabel = styled(FormControlLabel)({
   fontSize: "50px",
-  '& .MuiSvgIcon-root': {
-    color: '#4A90E2',
+  "& .MuiSvgIcon-root": {
+    color: "#4A90E2",
   },
-  '& .MuiTypography-root': {
-    color: '#333',
+  "& .MuiTypography-root": {
+    color: "#333",
     fontSize: "20px",
-    paddingRight:"30px"
-  }
+    paddingRight: "30px",
+  },
 });
 
 const style = {
@@ -68,33 +74,36 @@ const LoginPage = () => {
     setOpen(false);
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const payload = {
       email: universityId,
       password: password,
     };
-    dispatch(institution_login(payload, () => {
-      localStorage.setItem("branch", "All Branches");
-      localStorage.setItem("course", "All Courses");
-      localStorage.setItem("department", "All Departments");
-      localStorage.setItem("user", "All Users");
-      window.location.href = "./adminDashboard";
-    }));
+    dispatch(
+      institution_login(payload, () => {
+        localStorage.setItem("branch", "All Branches");
+        localStorage.setItem("course", "All Courses");
+        localStorage.setItem("department", "All Departments");
+        localStorage.setItem("user", "All Users");
+        window.location.href = "./adminDashboard";
+      })
+    );
   };
 
   const handleLoginTypeChange = (type) => {
     setLoginType(type);
-    if (type == 'student') {
-      navigate("/studentLogin")
+    if (type == "student") {
+      navigate("/studentLogin");
     }
   };
 
   const handleRadioChange = (event) => {
     const selectedType = event.target.value;
     setLoginType(selectedType);
-    if (selectedType === 'student') {
+    if (selectedType === "student") {
       navigate("/studentLogin");
-    } else if (selectedType === 'teacher') {
+    } else if (selectedType === "teacher") {
       navigate("/teacherLogin");
     }
     // Add more conditions for other types if necessary
@@ -102,66 +111,90 @@ const LoginPage = () => {
 
   return (
     <div className="p-4">
-
       <div className="flex flex-col md:flex-row">
         <div className="w-full md:w-3/6 p-5 md:p-20">
           <div className="bg-white rounded-lg p-6">
-
             <FormControl component="fieldset">
               {/* <FormLabel component="legend" style={{ textAlign: 'center', marginBottom: '10px' }}>Login Type</FormLabel> */}
-              <StyledRadioGroup row value={loginType} onChange={handleRadioChange}>
-                <StyledFormControlLabel value="institution" control={<Radio />} label="Institution" />
-                <StyledFormControlLabel value="student" control={<Radio />} label="Student" />
-                <StyledFormControlLabel value="teacher" control={<Radio />} label="Teacher" />
+              <StyledRadioGroup
+                row
+                value={loginType}
+                onChange={handleRadioChange}
+              >
+                <StyledFormControlLabel
+                  value="institution"
+                  control={<Radio />}
+                  label="Institution"
+                />
+                <StyledFormControlLabel
+                  value="student"
+                  control={<Radio />}
+                  label="Student"
+                />
+                <StyledFormControlLabel
+                  value="teacher"
+                  control={<Radio />}
+                  label="Teacher"
+                />
               </StyledRadioGroup>
             </FormControl>
 
-            <div className="mb-4">
-              <h2 className="text-2xl font-semibold  mb-4">Institution Login</h2>
-              {/* <p className="text-base text-gray-600 mb-7">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <h2 className="text-2xl font-semibold  mb-4">
+                  Institution Login
+                </h2>
+                {/* <p className="text-base text-gray-600 mb-7">
                 Get access to our expert insights about your students across departments, branches, and cities today!
               </p> */}
-            </div>
+              </div>
+              <div className="space-y-4">
+                <div className="mb-2">
+                  <label
+                    htmlFor="universityId"
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    University ID
+                  </label>
+                  <input
+                    type="text"
+                    className="mt-2 w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-400"
+                    id="universityId"
+                    name="universityId"
+                    placeholder=""
+                    value={universityId}
+                    onChange={loginInputHandler}
+                  />
+                </div>
+                <div className="mb-2">
+                  <label
+                    htmlFor="password"
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="mt-2 w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-400"
+                    id="password"
+                    name="password"
+                    placeholder=""
+                    value={password}
+                    onChange={loginInputHandler}
+                  />
+                </div>
+              </div>
 
-            <div className="space-y-4">
-              <div className="mb-2">
-                <label htmlFor="universityId" className="text-sm font-medium text-gray-600">
-                  University ID
-                </label>
-                <input
-                  type="text"
-                  className="mt-2 w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-400"
-                  id="universityId"
-                  name="universityId"
-                  placeholder=""
-                  value={universityId}
-                  onChange={loginInputHandler}
-                />
+              <div className="mt-4">
+                <button
+                  type="submit"
+                  className="w-full bg-blue-500 text-white rounded-md py-2 px-4 hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                >
+                  Login
+                </button>
               </div>
-              <div className="mb-2">
-                <label htmlFor="password" className="text-sm font-medium text-gray-600">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  className="mt-2 w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-400"
-                  id="password"
-                  name="password"
-                  placeholder=""
-                  value={password}
-                  onChange={loginInputHandler}
-                />
-              </div>
-            </div>
-            <div className="mt-4">
-              <button
-                type="button"
-                className="w-full bg-blue-500 text-white rounded-md py-2 px-4 hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-                onClick={handleSubmit}
-              >
-                Login
-              </button>
-            </div>
+            </form>
+
             {/* <div className="mt-7 text-center">
               <span className="text-gray-600">
                 Not a member with us yet? {" "}
@@ -173,32 +206,39 @@ const LoginPage = () => {
 
             <div className="student-login-section bg-gradient-to-r from-blue-100 to-teal-100 p-5 rounded-lg shadow-md mt-5">
               <div className="flex items-center justify-center">
-                <h2 className="text-lg font-semibold text-gray-700 mr-4">Welcome !</h2>
-                <Link to={"/registration"} className="text-white font-bold py-1 px-3 rounded-full transition duration-300 transform hover:scale-110 bg-gradient-to-r from-blue-500 to-teal-400 hover:from-blue-600 hover:to-teal-500 shadow-lg hover:shadow-xl">
+                <h2 className="text-lg font-semibold text-gray-700 mr-4">
+                  Welcome !
+                </h2>
+                <Link
+                  to={"/registration"}
+                  className="text-white font-bold py-1 px-3 rounded-full transition duration-300 transform hover:scale-110 bg-gradient-to-r from-blue-500 to-teal-400 hover:from-blue-600 hover:to-teal-500 shadow-lg hover:shadow-xl"
+                >
                   Join Us Today!
                 </Link>
               </div>
             </div>
             <div className="mt-10 text-center text-gray-600">
-              <span>
-                By continuing, you agree to our
-              </span>
-              <a href="https://www.munsow.com/terms-and-conditions" target="_blank" className="font-semibold text-blue-500 ml-1">
+              <span>By continuing, you agree to our</span>
+              <a
+                href="https://www.munsow.com/terms-and-conditions"
+                target="_blank"
+                className="font-semibold text-blue-500 ml-1"
+              >
                 Terms of Service
               </a>
               <span> and </span>
-              <a href="https://www.munsow.com/privacy-policy" target="_blank" className="font-semibold text-blue-500">
+              <a
+                href="https://www.munsow.com/privacy-policy"
+                target="_blank"
+                className="font-semibold text-blue-500"
+              >
                 Privacy Policy
               </a>
             </div>
           </div>
         </div>
         <div className="hidden md:inline w-3/6">
-          <img
-            src={interview}
-            className="w-full p-5 md:p-20"
-            alt="Login"
-          />
+          <img src={interview} className="w-full p-5 md:p-20" alt="Login" />
         </div>
       </div>
     </div>
