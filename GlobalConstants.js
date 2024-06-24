235df95d5e279053ad2487a245d0eda5ff1c78c2
Ram T <ramt@Rams-MacBook-Air.local>
2024-06-24 18:24:42 +0530

    env changes

--- GlobalConstants.js
+++ GlobalConstants.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const GLOBAL_CONSTANTS = {
   // backend_url: import.meta.env.VITE_BACKEND_API_URL,
-  backend_url: "https://munsow-stg-e77188fb3b91.herokuapp.com/",
+  backend_url: "https://munsow-66843b41624e.herokuapp.com/",
 
   loggedIn:JSON.parse(localStorage?.getItem("user_data")) ? true : false,
   user_cred:JSON.parse(localStorage?.getItem("user_data"))?.data,
