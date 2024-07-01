c0563a0130167aebaaf24586d809ab7dbfbed4d0
Ram T <ramt@Rams-MacBook-Air.local>
2024-07-01 18:41:12 +0530

    added changes

--- src/screens/Quiz/StudentQuizView/StudentQuizList.jsx
+++ src/screens/Quiz/StudentQuizView/StudentQuizList.jsx
@@ -19,7 +19,7 @@ import { deleteQuiz, loadAssignments, loadQuestionBanks } from '../../../redux/a
 import { useDarkMode } from "./../../../Dark";
 
 
-const QuizCard = ({ quiz, onClick,onClick1, title }) => {
+const QuizCard = ({ quiz, onClick, onClick1, title }) => {
     const dispatch = useDispatch();
     const controls = useAnimation();
     const [open, setOpen] = useState(false);
@@ -35,7 +35,7 @@ const QuizCard = ({ quiz, onClick,onClick1, title }) => {
 
     const handleResultsClick = async () => {
         await controls.start({ opacity: 0.9, scale: 0.95 });
-        onClick1(quiz.id);
+        onClick1(quiz.result_id);
     };
 
     const handleOpen = () => {
@@ -68,47 +68,82 @@ const QuizCard = ({ quiz, onClick,onClick1, title }) => {
 
     return (
         <motion.div
-        className={`w-full h-auto mx-auto inline-block rounded-lg overflow-hidden cursor-pointer 
+            className={`w-full h-auto mx-auto inline-block rounded-lg overflow-hidden cursor-pointer 
         shadow-md bg-[#F0F0F0] hover:shadow-2xl transition-shadow duration-300 transform ${quiz.is_locked ? 'opacity-50 pointer-events-none' : 'hover:scale-105'
-            }`}
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
-    >
-        <div className="p-3 flex flex-col justify-between h-full">
-            <div className="justify-between mb-2">
-                <h2 className="text-lg font-bold mb-2">{quiz.name}</h2>
-                <p className="text-gray-600 text-md my-4">{quiz.description}</p>
-                <p className="text-gray-600 text-md">Deadline : {quiz.deadline}</p>
-                <p className="text-gray-600 text-md">Skills Required : {quiz?.skills_required}</p>
-                <p className="text-gray-600 text-md">Minutes Allowed : {quiz.max_time_min}</p>
-            </div>
-            {quiz.is_locked && quiz.allows_late_submission && (
-                <div className="text-red-500 text-sm mt-2 md:mt-0 md:ml-2">
-                    Late submission is allowed.
+                }`}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+        >
+            <div className="p-4 flex flex-col justify-between h-full">
+                <div className="justify-between mb-2">
+                    <h2 className="text-lg font-bold mb-2">{quiz.name}</h2>
+                    <p className="text-gray-600 text-md my-4">{quiz.description}</p>
+                    {quiz.deadline != null && (<p className="text-gray-600 text-md">Deadline : {quiz.deadline}</p>)}
+                    <p className="text-gray-600 text-md">Skills Required : {quiz?.skills_required}</p>
+                    <p className="text-gray-600 text-md">Minutes Allowed : {quiz.max_time_min}</p>
                 </div>
-            )}
-            <div className="flex flex-col justify-between items-center p-4 border rounded-lg shadow-md bg-white md:flex-row">
-              {quiz.status !="Evaluation completed" && (  <div className="flex flex-col items-center mb-4 md:flex-row md:mb-0">
-                    <div className='flex text-md mr-4'>
-                        {quiz.questions?.length} Questions
+                {quiz.is_locked && quiz.allows_late_submission && (
+                    <div className="text-red-500 text-sm mt-2 md:mt-0 md:ml-2">
+                        Late submission is allowed.
                     </div>
-                </div>)}
-                <div>
-                    {quiz.status === 'Evaluation completed' ? (
-                        <motion.button
-                            whileHover={{ scale: 1.05 }}
-                            whileTap={{ scale: 0.95 }}
-                             onClick={handleResultsClick}
-                            style={{
-                                backgroundColor: linearGradientBackground,
-                                color: textColor
-                            }}
-                            className="px-4 py-2 font-bold shadow-xl rounded-md transition duration-300 focus:outline-none focus:ring focus:ring-blue-300"
-                        >
-                            See Results
-                        </motion.button>
-                    ) : quiz.is_locked ? (
-                        quiz.allows_late_submission ? (
+                )}
+                <div className="flex flex-col justify-between items-center p-2 border rounded-lg shadow-md bg-white md:flex-row">
+
+                    {quiz.status != "Evaluation completed" && (<div className="flex flex-col items-center mb-4 md:flex-row md:mb-0">
+                        <div className='flex text-md mr-4'>
+                            {quiz.questions?.length} Questions
+                        </div>
+                    </div>)}
+
+
+
+
+
+
+                    <div>
+                        {quiz.status === 'Evaluation completed' ? (
+                            <motion.button
+                                whileHover={{ scale: 1.05 }}
+                                whileTap={{ scale: 0.95 }}
+                                onClick={handleResultsClick}
+                                style={{
+                                    backgroundColor: linearGradientBackground,
+                                    color: textColor
+                                }}
+                                className="px-4 py-2 font-bold shadow-xl rounded-md transition duration-300 focus:outline-none focus:ring focus:ring-blue-300"
+                            >
+                                See Results
+                            </motion.button>
+                        ) : quiz.is_locked ? (
+                            quiz.allows_late_submission ? (
+                                <motion.button
+                                    whileHover={{ scale: 1.05 }}
+                                    whileTap={{ scale: 0.95 }}
+                                    onClick={handleCardClick}
+                                    style={{
+                                        backgroundColor: linearGradientBackground,
+                                        color: textColor
+                                    }}
+                                    className="px-4 py-2 font-bold shadow-xl rounded-md transition duration-300 focus:outline-none focus:ring focus:ring-blue-300"
+                                >
+                                    Start
+                                </motion.button>
+                            ) : (
+                                <motion.button
+                                    whileHover={{ scale: 1.05 }}
+                                    whileTap={{ scale: 0.95 }}
+                                    style={{
+                                        backgroundColor: linearGradientBackground,
+                                        color: textColor,
+                                        cursor: 'not-allowed'
+                                    }}
+                                    className="px-4 py-2 shadow-xl font-bold rounded-md transition duration-300 focus:outline-none"
+                                    disabled
+                                >
+                                    Locked
+                                </motion.button>
+                            )
+                        ) : (
                             <motion.button
                                 whileHover={{ scale: 1.05 }}
                                 whileTap={{ scale: 0.95 }}
@@ -119,65 +154,54 @@ const QuizCard = ({ quiz, onClick,onClick1, title }) => {
                                 }}
                                 className="px-4 py-2 font-bold shadow-xl rounded-md transition duration-300 focus:outline-none focus:ring focus:ring-blue-300"
                             >
-                                Startj
+                                Start
                             </motion.button>
-                        ) : (
+                        )}
+
+                        {quiz.completed_attempt < quiz.number_of_reattempt && (
                             <motion.button
                                 whileHover={{ scale: 1.05 }}
                                 whileTap={{ scale: 0.95 }}
+                                onClick={handleCardClick}
                                 style={{
                                     backgroundColor: linearGradientBackground,
                                     color: textColor,
-                                    cursor: 'not-allowed'
+                                    marginLeft:"10px"
                                 }}
-                                className="px-4 py-2 shadow-xl font-bold rounded-md transition duration-300 focus:outline-none"
-                                disabled
+                                className="px-4 py-2 font-bold shadow-xl rounded-md transition duration-300 focus:outline-none focus:ring focus:ring-blue-300"
                             >
-                                Locked
+                                ReAttempt
                             </motion.button>
-                        )
-                    ) : (
-                        <motion.button
-                            whileHover={{ scale: 1.05 }}
-                            whileTap={{ scale: 0.95 }}
-                            onClick={handleCardClick}
-                            style={{
-                                backgroundColor: linearGradientBackground,
-                                color: textColor
-                            }}
-                            className="px-4 py-2 font-bold shadow-xl rounded-md transition duration-300 focus:outline-none focus:ring focus:ring-blue-300"
-                        >
-                            Start
-                        </motion.button>
-                    )}
+                        )}
+
+                    </div>
                 </div>
             </div>
-        </div>
-    
-        <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
-            <DialogTitle style={{ textAlign: 'center', fontWeight: 'bold' }}>
-                {"Delete Assignment?"}
-            </DialogTitle>
-            <DialogContent>
-                <DialogContentText style={{ margin: '20px', fontSize: '16px', textAlign: 'center' }}>
-                    Are you sure you want to delete this Assignment?
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions style={{ justifyContent: 'center' }}>
-                <Button onClick={handleClose} variant="outlined" color="primary" style={{ margin: '10px' }}>
-                    Cancel
-                </Button>
-                <Button onClick={handleDeleteConfirm} variant="contained" color="error" startIcon={<DeleteIcon />} style={{ margin: '10px' }} autoFocus>
-                    Delete
-                </Button>
-            </DialogActions>
-        </Dialog>
-    </motion.div>
-    
+
+            <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
+                <DialogTitle style={{ textAlign: 'center', fontWeight: 'bold' }}>
+                    {"Delete Assignment?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText style={{ margin: '20px', fontSize: '16px', textAlign: 'center' }}>
+                        Are you sure you want to delete this Assignment?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions style={{ justifyContent: 'center' }}>
+                    <Button onClick={handleClose} variant="outlined" color="primary" style={{ margin: '10px' }}>
+                        Cancel
+                    </Button>
+                    <Button onClick={handleDeleteConfirm} variant="contained" color="error" startIcon={<DeleteIcon />} style={{ margin: '10px' }} autoFocus>
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </motion.div>
+
     );
 };
 
-const ScrollableContainer = ({ title, quizzes, handleCardClick,handleResultsClick, textColor, visibleCount }) => {
+const ScrollableContainer = ({ title, quizzes, handleCardClick, handleResultsClick, textColor, visibleCount }) => {
     const [visibleStartIndex, setVisibleStartIndex] = useState(0);
     const [showAll, setShowAll] = useState(false);
 
@@ -202,7 +226,7 @@ const ScrollableContainer = ({ title, quizzes, handleCardClick,handleResultsClic
     };
 
     return (
-        <div className="max-w-7xl p-8">
+        <div className="max-w-7xl ">
             <div className="mb-6 flex items-center justify-between">
                 <div className='flex'>
                     <h1 className="text-2xl font-bold">{title}</h1>
@@ -259,7 +283,7 @@ const ScrollableContainer = ({ title, quizzes, handleCardClick,handleResultsClic
                         className={`grid ${title === "Continue Learning..." ? 'grid-cols-4' : 'grid-cols-4'} gap-8`}
                     >
                         {quizzes.map((quiz) => (
-                            <QuizCard key={quiz?.id} quiz={quiz} onClick={handleCardClick} onClick1={handleResultsClick} 
+                            <QuizCard key={quiz?.id} quiz={quiz} onClick={handleCardClick} onClick1={handleResultsClick}
                                 title={title} />
                         ))}
                     </motion.div>
@@ -322,7 +346,7 @@ const StudentQuizList = () => {
     };
 
     return (
-        <div className="max-w-7xl mx-auto p-8">
+        <div className="max-w-8xl mx-auto p-8">
             <div className='flex justify-end'>
                 <div className={` px-8 py-4 flex rounded-lg gap-6`}
                     style={{
