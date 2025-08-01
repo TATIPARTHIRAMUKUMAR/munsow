91be58666167a15d40c64a1a72a686a95cfbb076
sailikhithk <sailikhithcse@gmail.com>
2025-08-01 00:12:20 -0500

    Remove trailing slash from backend_url constant
    Updated the backend_url in GlobalConstants.js to remove the trailing slash for consistency. Also updated caniuse-lite in package-lock.json.

--- GlobalConstants.js
+++ GlobalConstants.js
@@ -1,6 +1,6 @@
 const GLOBAL_CONSTANTS = {
   // backend_url: import.meta.env.VITE_BACKEND_API_URL,
-  backend_url: "https://demo.munsow.com/",
+  backend_url: "https://demo.munsow.com",
 
   loggedIn: JSON.parse(localStorage?.getItem("user_data")) ? true : false,
   user_cred: JSON.parse(localStorage?.getItem("user_data"))?.data,
